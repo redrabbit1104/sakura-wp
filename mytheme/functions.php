8690f455ac12658871925b6c44526d42374ec801
redrabbit1104 <redcaprabbit1104@gmail.com>
2021-04-28 14:05:05 +0900

    add_menu

--- mytheme/functions.php
+++ mytheme/functions.php
@@ -79,3 +79,27 @@ function mytheme_widgets() {
 
 }
 add_action( 'widgets_init', 'mytheme_widgets' );
+
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+    register_post_type( 'korean', [ // 投稿タイプ名の定義
+        'labels' => [
+            'name'          => '韓国語', // 管理画面上で表示する投稿タイプ名
+            'singular_name' => 'korean',    // カスタム投稿の識別名
+        ],
+        'public'        => true,  // 投稿タイプをpublicにするか
+        'has_archive'   => false, // アーカイブ機能ON/OFF
+        'menu_position' => 5,     // 管理画面上での配置場所
+        'show_in_rest'  => true,  // 5系から出てきた新エディタ「Gutenberg」を有効にする
+    ]);
+		register_post_type( 'travel', [ // 投稿タイプ名の定義
+			'labels' => [
+					'name'          => 'トラベル', // 管理画面上で表示する投稿タイプ名
+					'singular_name' => 'travel',    // カスタム投稿の識別名
+			],
+			'public'        => true,  // 投稿タイプをpublicにするか
+			'has_archive'   => false, // アーカイブ機能ON/OFF
+			'menu_position' => 6,     // 管理画面上での配置場所
+			'show_in_rest'  => true,  // 5系から出てきた新エディタ「Gutenberg」を有効にする
+	]);
+}
